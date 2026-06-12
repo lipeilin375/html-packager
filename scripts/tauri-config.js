27ac9b7bd7d4e425404bce82336b1e21b02dfa89
Billy Lin <74428719+lipeilin375@users.noreply.github.com>
2026-06-13 00:12:35 +0800

    Update tauri-config.js

--- scripts/tauri-config.js
+++ scripts/tauri-config.js
@@ -5,12 +5,14 @@ const config = {
   productName: process.env.PRODUCT_NAME,
   version: process.env.VERSION,
   identifier: process.env.IDENTIFIER,
+
   build: {
     frontendDist: "../dist"
   },
+
   bundle: {
     active: true,
-    targets: ["all"],
+    targets: "all",
     icon: [
       "icons/icon.png",
       "icons/icon.ico",
@@ -23,5 +25,4 @@ const outPath = path.resolve(__dirname, '../app/tauri.conf.json');
 
 fs.writeFileSync(outPath, JSON.stringify(config, null, 2));
 
-console.log("tauri.conf.json generated:");
-console.log(config);
+console.log("tauri.conf.json generated (v2 compatible)");
